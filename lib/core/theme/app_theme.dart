d8c59cfddf680f95e3f3330b5a273e2cec4b7382
IsabellaSGoncalves <isabelllacom2l@gmail.com>
2026-04-19 18:05:16 -0300

    refactor: font comfortaa in all pages

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'app_colors.dart';
 import 'app_text_styles.dart';
 
 class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
+      fontFamily: GoogleFonts.comfortaa().fontFamily,
+
+      textTheme: GoogleFonts.comfortaaTextTheme().copyWith(
+        titleLarge: AppTextStyles.title,
+        bodyMedium: AppTextStyles.body,
+      ),
+
       scaffoldBackgroundColor: AppColors.background,
 
       colorScheme: const ColorScheme.light(
@@ -14,11 +22,6 @@ class AppTheme {
         surface: AppColors.surface,
         onPrimary: AppColors.textLight,
         onSurface: AppColors.textDark,
-      ),
-
-      textTheme: TextTheme(
-        titleLarge: AppTextStyles.title,
-        bodyMedium: AppTextStyles.body,
       ),
 
       appBarTheme: const AppBarTheme(
@@ -41,47 +44,31 @@ class AppTheme {
       cardTheme: CardThemeData(
         color: AppColors.surface,
         elevation: 2,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
       ),
 
       inputDecorationTheme: InputDecorationTheme(
         filled: true,
         fillColor: AppColors.background,
-
         contentPadding: const EdgeInsets.symmetric(
           horizontal: 20,
           vertical: 16,
         ),
-
-        border: OutlineInputBorder(
+        border: const OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(30)),
           borderSide: BorderSide.none,
         ),
-
-        enabledBorder: OutlineInputBorder(
+        enabledBorder: const OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(30)),
-          borderSide: BorderSide(
-            color: AppColors.primary,
-            width: 2,
-          ),
+          borderSide: BorderSide(color: AppColors.primary, width: 2),
         ),
-
-        focusedBorder: OutlineInputBorder(
+        focusedBorder: const OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(30)),
-          borderSide: BorderSide(
-            color: AppColors.surface,
-            width: 2,
-          ),
+          borderSide: BorderSide(color: AppColors.surface, width: 2),
         ),
-
-        errorBorder: OutlineInputBorder(
+        errorBorder: const OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(30)),
-          borderSide: BorderSide(
-            color: AppColors.error,
-            width: 1,
-          ),
+          borderSide: BorderSide(color: AppColors.error, width: 1),
         ),
       ),
     );
